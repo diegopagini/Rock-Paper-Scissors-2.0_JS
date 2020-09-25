36ff898ca24d599f70b5e55755ad69840353142d
unknown <diegopagini777@gmail.com>
2020-09-25 16:17:59 -0300

    vamos con el js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const $PAPER = document.getElementById('paper');
-const $ROCK = document.getElementById('rock');
-const $SCISSORS = document.getElementById('scissors');
+const $PAPER = document.getElementById('paper').onclick = () => {
+  opcionUsuario = papel;
+}
+const $ROCK = document.getElementById('rock').onclick = () => {
+  opcionUsuario = piedra;
+}
+const $SCISSORS = document.getElementById('scissors').onclick = () => {
+  opcionUsuario = tijera;
+}
 
 function aleatorio() {
   opcionCPU = Math.floor(Math.random() * 3);
