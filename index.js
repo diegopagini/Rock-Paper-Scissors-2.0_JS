49901318ada242b5c70624b5a80ad39a544c0add
unknown <diegopagini777@gmail.com>
2020-09-25 15:59:37 -0300

    comenzando con js

---
+++ index.js
@@ -0,0 +1,7 @@
+const $PAPER = document.getElementById('paper');
+const $ROCK = document.getElementById('rock');
+const $SCISSORS = document.getElementById('scissors');
+
+function aleatorio() {
+  opcionCPU = Math.floor(Math.random() * 3);
+}
